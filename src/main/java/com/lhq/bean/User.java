d2deabee0613e7fe7c5a0e9501486da4be5e40ce
jiumozhi <jiumozhi@qq.com>
2023-02-07 17:00:31 +0800

    1.3-VERSION

--- src/main/java/com/lhq/bean/User.java
+++ src/main/java/com/lhq/bean/User.java
@@ -4,4 +4,10 @@ public class User {
     private Integer id;
     private String name;
     private String pwd;
+
+    public void move() {
+        System.out.println("move....");
+        System.out.println("小伙子...");
+        System.out.println("小伙子...");
+    }
 }
